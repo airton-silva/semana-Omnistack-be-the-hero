a4924bd2dd05e7d7684405c2540cd14937a27618
unknown <airtonsilvan89@gmail.com>
2020-06-20 21:36:37 -0300

    the be hero completo

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -1,14 +1,30 @@
 const express = require('express');
 const routes = require('./routes');
 const cors = require('cors');
+const {errors} = require('celebrate');
 
 const app = express();
 
 app.use(express.json());
 
-app.use(cors());
+
+var whitelist = ['http://127.0.0.1:3333']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+app.use(cors(whitelist));
+
 
 app.use(routes);
+
+app.use(errors());
 
 
 app.listen(3333);
--- back-end/src/routes.js
+++ back-end/src/routes.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const {celebrate, Segments, Joi}= require('celebrate');
+
 
 const ongController = require('./controllers/ongController');
 const IncidentController = require('./controllers/IncidentController');
 const ProfileController = require('./controllers/ProfileController');
 const SessionController = require('./controllers/SessionController');
+
 
 const connection = require('./database/connection');
 
@@ -12,9 +15,21 @@ const routes =  express.Router();
 routes.post('/session', SessionController.create);
 
 routes.get('/ongs', ongController.index);
-routes.post('/ongs', ongController.create);
+routes.post('/ongs', celebrate({
+    [Segments.BODY]:Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.number().required().min(10).max(11),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2),
+    })
+}), ongController.create);
 
-routes.get('/profile', ProfileController.index);
+routes.get('/profile', celebrate({
+    [Segments.HEADERS]:Joi.object({
+        authotization: joi.string().required(),
+    }).unknown(),
+}), ProfileController.index);
 
 routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.create);
